de585c99efcb05441d91bc6f4b40f42c4307ffeb
Abdullah Khan <34720974+abk231@users.noreply.github.com>
2019-11-04 03:16:07 -0800

    Add files via upload

---
+++ Decorator pattern/src/decorator/pattern/DecoratorPattern.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package decorator.pattern;
+
+/**
+ *
+ * @author imran
+ */
+public class DecoratorPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+
+      Shape redCircle = new RedShapeDecorator(new Circle());
+
+      Shape redRectangle = new RedShapeDecorator(new Rectangle());
+      System.out.println("Circle with normal border");
+      circle.draw();
+
+      System.out.println("\nCircle of red border");
+      redCircle.draw();
+
+      System.out.println("\nRectangle of red border");
+      redRectangle.draw();
+    }
+    
+}
