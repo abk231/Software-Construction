b1f889070f938cc978c6fbf2a0d97653c52002d8
Abdullah Khan <34720974+abk231@users.noreply.github.com>
2019-11-04 03:15:06 -0800

    Add files via upload

---
+++ ChainofResponsibilityDesignPattern/src/chainofrespdemo/ErrorLogger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chainofrespdemo;
+
+/**
+ *
+ * @author fa16-bse-090
+ */
+public class ErrorLogger extends AbstractLogger{
+    
+    public ErrorLogger(int level){
+      this.level = level;
+   }
+
+   @Override
+   protected void write(String message) {		
+      System.out.println("Error Console::Logger: " + message);
+   }
+}
---
+++ ChainofResponsibilityDesignPattern/src/chainofrespdemo/FileLogger.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chainofrespdemo;
+
+/**
+ *
+ * @author fa16-bse-090
+ */
+public class FileLogger extends AbstractLogger{
+    public FileLogger(int level){
+      this.level = level;
+   }
+
+   @Override
+   protected void write(String message) {		
+      System.out.println("File::Logger: " + message);
+   }
+    
+}
